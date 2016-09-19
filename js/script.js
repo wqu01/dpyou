53e49379306b32dbdb42c33346fa510eb4748aa5
Wenniao Qu <wenniao.qu@DPPS-MBA123.local>
2016-09-19 17:26:14 -0400

    added timeline

--- js/script.js
+++ js/script.js
@@ -10,14 +10,14 @@ $( document ).ready(function() {
     var countdown = document.getElementById("countdown");
      
     // update the tag with id "countdown" every 1 second
-    //setInterval(function () {
+    setInterval(function () {
     // find the amount of "seconds" between now and target
     var current_date = new Date().getTime();
     var seconds_left = (target_date - current_date) / 1000;
  
     // do some time calculations
     days = parseInt(seconds_left / 86400);
-    /*
+    
     seconds_left = seconds_left % 86400;
      
     hours = parseInt(seconds_left / 3600);
@@ -25,13 +25,13 @@ $( document ).ready(function() {
      
     minutes = parseInt(seconds_left / 60);
     seconds = parseInt(seconds_left % 60);
-     */
+     
 
     // format countdown string + set tag value
-    /* countdown.innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";  */
-    countdown.innerHTML = days; 
-    //}, 1000);
+     countdown.innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";  
+    //countdown.innerHTML = days; 
+    }, 1000);
 
 
 
