61ab15601404161c28cf505fadd971b812c6233d
Wenniao Qu <wenniao.qu@DPPS-MBA123.local>
2016-10-03 10:37:09 -0400

    added forms, sentences, more mobile friendly

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 
-
-/*
-$(function(){
-    window.sr = ScrollReveal();
-    sr.reveal('.lines');
-});
-*/
-
 $( document ).ready(function() {
-    
-   
-
     $(function (){
 
     //if($(window).width() > 1023){
@@ -40,12 +29,8 @@ $( document ).ready(function() {
                 });*/
             }});
     //}
-
-
     });
     
-  
-
     // set the date we're counting down to
     var target_date = new Date("Oct 28, 2016").getTime();
      
@@ -59,26 +44,44 @@ $( document ).ready(function() {
     // update the tag with id "countdown" every 1 second
     setInterval(function () {
     // find the amount of "seconds" between now and target
-    var current_date = new Date().getTime();
-    var seconds_left = (target_date - current_date) / 1000;
- 
-    // do some time calculations
-    days = parseInt(seconds_left / 86400);
-    
-    seconds_left = seconds_left % 86400;
+        var current_date = new Date().getTime();
+        var seconds_left = (target_date - current_date) / 1000;
      
-    hours = parseInt(seconds_left / 3600);
-    seconds_left = seconds_left % 3600;
-     
-    minutes = parseInt(seconds_left / 60);
-    seconds = parseInt(seconds_left % 60);
-     
+        // do some time calculations
+        days = parseInt(seconds_left / 86400);
+        
+        seconds_left = seconds_left % 86400;
+         
+        hours = parseInt(seconds_left / 3600);
+        seconds_left = seconds_left % 3600;
+         
+        minutes = parseInt(seconds_left / 60);
+        seconds = parseInt(seconds_left % 60);
+         
 
-    // format countdown string + set tag value
-    // countdown.innerHTML = days + "d " + hours + "h "
-    //+ minutes + "m " + seconds + "s ";  
-    countdown.innerHTML = days; 
-    
+        // format countdown string + set tag value
+        // countdown.innerHTML = days + "d " + hours + "h "
+        //+ minutes + "m " + seconds + "s ";  
+        countdown.innerHTML = days; 
     }, 1000);
+
+    $( "#food-header" ).click(function() {
+      $( "#food-form" ).slideToggle();
+      $( "#food-header > .button-expand" ).toggle();
+      $( "#food-header > .button-minus" ).toggle();
+
+
+
+    });
+
+    $( "#music-header" ).click(function() {
+      $( "#music-form" ).slideToggle();
+      
+      $( "#music-header > .button-expand" ).toggle();
+      $( "#music-header > .button-minus" ).toggle();
+       
+
+    });
+
 });
 
